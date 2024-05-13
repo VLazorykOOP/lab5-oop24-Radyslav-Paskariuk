9cc5023d126bd0100bae480bc4dc736417d0c85b
Radyslav-Paskariuk <paskariuk.radyslav@chnu.edu.ua>
2024-05-13 15:32:59 +0300

    first

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -3,22 +3,80 @@
 
 
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 // Ваші файли загловки 
 //
 #include "Lab5Exmaple.h"
+
+// #1
+class Liquid {
+protected:
+    string name;
+    double density;
+
+public:
+    Liquid(string _name, double _density) : name(_name), density(_density) {}
+    Liquid() : name(""), density(0.0) {}
+
+
+    ~Liquid() {}
+
+
+    void print() {
+        cout << "Liquid: " << name << ", Density: " << density << endl;
+    }
+
+
+    void setDensity(double _density) {
+        density = _density;
+    }
+};
+
+
+class AlcoholicLiquid : public Liquid {
+private:
+    double strength;
+
+public:
+
+    AlcoholicLiquid(string _name, double _density, double _strength) : Liquid(_name, _density), strength(_strength) {}
+
+    AlcoholicLiquid() : Liquid(), strength(0.0) {}
+
+
+    ~AlcoholicLiquid() {}
+
+
+    void print() {
+        cout << "Alcoholic Liquid: " << name << ", Density: " << density << ", Strength: " << strength << "%" << endl;
+    }
+
+
+    void setStrength(double _strength) {
+        strength = _strength;
+    }
+};
+
 int main()
 {
     std::cout << " Lab #5  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
 
-    int chain = 1;
-    if (chain == 1) chain = mainExample1();
-    if (chain == 2) chain = mainExample2();
-    if (chain == 3) chain = mainExample3();
-    if (chain == 4) chain = mainExample4();
-    if (chain == 5) chain = mainExample5();
+    // #1
+    Liquid beer("Пиво", 0.98);
+    AlcoholicLiquid wine("Біле вино", 0.95, 12.5);
+
+    beer.print();
+    wine.print();
+
+
+    beer.setDensity(1.01);
+    wine.setStrength(13.0);
+
+    cout << "\nUpdated data:" << endl;
+    beer.print();
+    wine.print();
 
 }
